c5ad92450fcb2acb117b21f65d52ba4e06487708
Codie Sharp <codie.sharp@gmail.com>
2022-08-31 22:54:16 -0400

    initial commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,13 +8,12 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
+    path: "/beanstalk",
+    name: "beanstalk",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import("../views/BeanstalkView.vue"),
   },
 ];
 
---
+++ src/views/BeanstalkView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>This page will host my tactics game! ⚔️</h1>
+  </div>
+</template>
